11df2b4ce96da964bb6b9a6c7c042765057017f7
shtwangy <shtwangy@gmail.com>
2021-02-06 03:28:37 +0900

    feat(App): display page title and url

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import {useState, useEffect} from "react";
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="app">
-      <h1>Chrome Tab Manager</h1>
-    </div>
-  );
+    const [title, setTitle] = useState<string>('')
+    const [url, setUrl] = useState<string>('')
+    useEffect(() => {
+        const queryInfo = {active: true, lastFocusedWindow: true};
+
+        chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+            const title = tabs[0].title;
+            setTitle(title ? title : '')
+            const url = tabs[0].url;
+            setUrl(url ? url : '');
+        });
+    }, []);
+    return (
+        <div className="app">
+            <h1>{title}</h1>
+            <p>{url}</p>
+        </div>
+    );
 }
 
 export default App;
