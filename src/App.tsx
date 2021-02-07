bccab0327e3f7ab4b3b907f24b5cd81934223c65
shtwangy <shtwangy@gmail.com>
2021-02-07 10:24:56 +0900

    chore(src/App): init pages with mock data to verify operation

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import {useState, useEffect} from "react";
 import './App.css';
 import {Page} from "./types/Page";
 import {PageListItem} from "./components";
+import mockPages from "./mock/mock";
 
 const App = () => {
-    const [pages, setPages] = useState<Page[]>([])
+    // const [pages, setPages] = useState<Page[]>([])
+    const [pages, setPages] = useState<Page[]>(mockPages) // mock fro dev
     useEffect(() => {
         chrome.tabs && chrome.tabs.query({currentWindow: true}, tabs => {
             setPages(tabs.map(tab => {return {title: tab.title, url: tab.url}}))
