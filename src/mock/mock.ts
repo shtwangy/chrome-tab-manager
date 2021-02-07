3c5cf6335f48c7057a3f008ab53f980a0bc6e976
shtwangy <shtwangy@gmail.com>
2021-02-07 10:23:00 +0900

    test(src/mock): add pages mock data

---
+++ src/mock/mock.ts
@@ -0,0 +1,11 @@
+import {Page} from "../types";
+
+const mockPages: Page[] = new Array(10).fill(1).map((v, i) => (
+    {
+        title: `Hoge${i}`,
+        url: `https://hoge.com/${i}`
+    }
+))
+
+export default mockPages
+
