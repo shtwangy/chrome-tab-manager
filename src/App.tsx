1415f4667a5b51f25537b4c30b02770bcd3c76a4
shtwangy <shtwangy@gmail.com>
2021-02-12 23:42:46 +0900

    feat(src/components/Header/SettingBalloon): webhook url can be set in setting balloon
    Issues #8

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, createContext} from "react";
 import './App.css';
 import {Tab} from "./types/Tab";
 import {Header, PageList} from "./components";
 import mockPages from "./mock/mock";
+import {localStorageContext, useLocalStorage} from "./contexts/useLocalStorage";
 
 const App = () => {
     // const [pages, setPages] = useState<Tab[]>([])
     const [tabs, setTabs] = useState<Tab[]>(mockPages) // mock fro dev
+    const ctx = useLocalStorage()
+
     useEffect(() => {
         chrome.tabs && chrome.tabs.query({currentWindow: true}, tabs => {
             console.log(tabs)
@@ -25,11 +28,13 @@ const App = () => {
 
     return (
         <div className="app">
-            <Header />
-            <PageList
-                tabs={tabs}
-                setTabs={setTabs}
-            />
+            <localStorageContext.Provider value={ctx}>
+                <Header />
+                <PageList
+                    tabs={tabs}
+                    setTabs={setTabs}
+                />
+            </localStorageContext.Provider>
         </div>
     );
 }
--- src/components/Header/SettingBalloon/index.tsx
+++ src/components/Header/SettingBalloon/index.tsx
@@ -1,10 +1,34 @@
-import {Ref, forwardRef} from "react";
+import {Ref, forwardRef, FormEvent, useState, useContext, useEffect} from "react";
 import {StyledWrapDiv} from "./style";
+import {localStorageContext} from "../../../contexts/useLocalStorage";
 
 const SettingBalloon = forwardRef<HTMLDivElement>(({}, ref: Ref<HTMLDivElement>) => {
+    const [webhookUrlText, setWebhookUrlText] = useState('')
+    const {webhookUrl, updateWebhookUrl} = useContext(localStorageContext)
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        updateWebhookUrl(webhookUrlText)
+    }
+
+    useEffect(() => {
+        setWebhookUrlText(webhookUrl)
+    }, [webhookUrl])
+
     return (
         <StyledWrapDiv ref={ref}>
-            Setting
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Webhook URL:
+                    <input
+                        name='webhook-url-text'
+                        type='text'
+                        value={webhookUrlText}
+                        onChange={e => setWebhookUrlText(e.target.value)}
+                    />
+                </label>
+                <input type="submit" value="Update Setting" />
+            </form>
         </StyledWrapDiv>
     )
 })
