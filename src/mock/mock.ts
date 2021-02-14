4b4a1d8813328eecff883d34f5f88b0fb7931f93
shtwangy <shtwangy@gmail.com>
2021-02-14 20:57:34 +0900

    chore(mock): update mock data

--- src/mock/mock.ts
+++ src/mock/mock.ts
@@ -1,6 +1,6 @@
-import {Page} from "../types";
+import {Tab} from "../types";
 
-const mockPages: Page[] = new Array(10).fill(1).map((v, i) => (
+const mockPages: Tab[] = new Array(10).fill(1).map((v, i) => (
     {
         id: i,
         index: i,
