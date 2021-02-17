1b0b56a018dd762273238a169dad414a56d5f1c7
shtwangy <shtwangy@gmail.com>
2021-02-17 23:24:17 +0900

    perf(UIKit/IconButton): memorizing component. add temporary debug code
    Issues #13

--- src/components/UIKit/IconButton/index.tsx
+++ src/components/UIKit/IconButton/index.tsx
@@ -1,20 +1,22 @@
-import {forwardRef, Ref} from "react";
+import {forwardRef, Ref, memo} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {StyledButton} from "./style";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
 
 type Props = {
+    id: string
     icon: IconProp
     onClick: () => void
 }
 
-const IconButton = forwardRef((props: Props, ref: Ref<HTMLButtonElement>) => {
-    const {icon, onClick} = props
+const IconButton = memo(forwardRef((props: Props, ref: Ref<HTMLButtonElement>) => {
+    const {icon, onClick, id} = props
+    console.log(`render IconButton ${id}`);
     return (
         <StyledButton ref={ref} onClick={onClick}>
             <FontAwesomeIcon icon={icon} fixedWidth />
         </StyledButton>
     )
-})
+}))
 
 export default IconButton
