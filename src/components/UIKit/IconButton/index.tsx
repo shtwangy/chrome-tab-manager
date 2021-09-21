cb611fcde7247255ae5ba8be677ccc2748240c85
shtwangy <shtwangy@gmail.com>
2021-09-21 18:27:20 +0900

    chore: remove unnecessary console log

--- src/components/UIKit/IconButton/index.tsx
+++ src/components/UIKit/IconButton/index.tsx
@@ -10,20 +10,15 @@ type Props = {
 }
 
 const IconButton = memo(forwardRef((props: Props, ref: Ref<HTMLButtonElement>) => {
-    // const [wobble, setWobble] = useState(false)
     const {icon, onClick, id} = props
 
     const onClickButton = () => {
         onClick()
-        // setWobble(true)
-        // setTimeout(() => setWobble(false), 20000);
     }
-    console.log(`render IconButton ${id}`);
     return (
         <StyledButton
             ref={ref}
             onClick={onClickButton}
-            // className={wobble ? 'wob' : ''}
         >
             <FontAwesomeIcon icon={icon} fixedWidth />
         </StyledButton>
