d6d5c45e8e1332d223203b4f81c58acf862c4d88
shtwangy <shtwangy@gmail.com>
2021-09-21 18:27:27 +0900

    chore: remove unnecessary console log

--- src/components/UIKit/SecondaryActionContainer/index.tsx
+++ src/components/UIKit/SecondaryActionContainer/index.tsx
@@ -2,7 +2,6 @@ import {StyledSecondaryActionDiv} from "./style";
 import {FC, memo} from "react";
 
 export const SecondaryActionContainer: FC = memo(({children}) => {
-    console.log('render secondary action')
     return(
         <StyledSecondaryActionDiv>
             {children}
